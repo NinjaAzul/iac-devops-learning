3a4dcab6a8c15887457f8f1ce1e0d90d62528f4e
Erick Freitas <erick.freitas@trin.ca>
2025-01-15 20:14:18 -0300

    configure backend

--- main.tf
+++ main.tf
@@ -1,12 +1,41 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.49.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-state-b55b5d5e39becea593186ede9c5ae085"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
+
 provider "aws" {
-  profile = "erick"
-  region  = "us-east-2"
+  region = "us-east-2"
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "terraform-state-b55b5d5e39becea593186ede9c5ae085"
+  force_destroy = true
+
+  provider = aws
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "aws_account_id" {
   type        = string
   sensitive   = true
 }
+
+variable "environment" {
+  description = "Ambiente de execução (local ou cloud)"
+  type        = string
+  default     = "local"
+}
